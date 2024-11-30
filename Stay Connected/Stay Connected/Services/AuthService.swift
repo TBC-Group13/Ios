bab3510b50e6a835e2bd0cd4b53fb0e901bddcb3
Rivianwolf <58031452+Rivianwolf@users.noreply.github.com>
2024-12-01 01:08:55 +0400

    Revert "Added mock data"

--- Stay Connected/Stay Connected/Services/AuthService.swift
+++ Stay Connected/Stay Connected/Services/AuthService.swift
@@ -1,21 +1,21 @@
-////
-////  AuthSerive.swift
-////  Stay Connected
-////
-////  Created by Giorgi Matiashvili on 30.11.24.
-////
 //
-//import Foundation
+//  AuthSerive.swift
+//  Stay Connected
 //
-//class AuthService {
-//    func login(phoneNumber: String, password: String, completion: @escaping (Bool) -> Void) {
-//        // Simulated API call
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-//            if phoneNumber == "123456789" && password == "password" {
-//                completion(true)
-//            } else {
-//                completion(false)
-//            }
-//        }
-//    }
-//}
+//  Created by Giorgi Matiashvili on 30.11.24.
+//
+
+import Foundation
+
+class AuthService {
+    func login(phoneNumber: String, password: String, completion: @escaping (Bool) -> Void) {
+        // Simulated API call
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            if phoneNumber == "123456789" && password == "password" {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+}
